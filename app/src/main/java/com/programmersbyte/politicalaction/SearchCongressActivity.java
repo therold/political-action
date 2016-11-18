26c0835a920ed4c950fe8e9ea1ceca72357ba0eb
Timothy Herold <tim@programmersbyte.com>
2016-11-18 12:37:01 -0800

    Implement View.OnClickListener for Search activity.

--- app/src/main/java/com/programmersbyte/politicalaction/SearchCongressActivity.java
+++ app/src/main/java/com/programmersbyte/politicalaction/SearchCongressActivity.java
@@ -2,13 +2,14 @@ package com.programmersbyte.politicalaction;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class SearchCongressActivity extends AppCompatActivity {
+public class SearchCongressActivity extends AppCompatActivity implements View.OnClickListener {
     @Bind(R.id.editSearchCongress) EditText mEditSearchCongress;
     @Bind(R.id.buttonSubmitSearch) Button mButtonSubmitSearch;
 
@@ -17,5 +18,19 @@ public class SearchCongressActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_congress);
         ButterKnife.bind(this);
+
+        mButtonSubmitSearch.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == mButtonSubmitSearch) {
+            String name = mEditSearchCongress.getText().toString().trim();
+            if(name.equals("")) {
+                mEditSearchCongress.setError("Field Required!");
+            } else {
+                //TODO: Display results
+            }
+        }
     }
 }
