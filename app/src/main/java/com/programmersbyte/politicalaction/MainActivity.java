c44855d7245dc81cf32b42167c0ca53fd6f1aabd
Timothy Herold <tim@programmersbyte.com>
2016-11-18 10:10:02 -0800

    Bind all TextViews in MainActivty using ButterKnife.

--- app/src/main/java/com/programmersbyte/politicalaction/MainActivity.java
+++ app/src/main/java/com/programmersbyte/politicalaction/MainActivity.java
@@ -2,12 +2,20 @@ package com.programmersbyte.politicalaction;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.textAllCongressmenLink) TextView mAllCongressmenLink;
+    @Bind(R.id.textAllSenatorsLink) TextView mAllSenatorsLink;
+    @Bind(R.id.textSearchCongressLink) TextView mSearchCongressLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
