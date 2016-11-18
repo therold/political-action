696d41c57ecbfdfbb29494a9838408b0df50f461
Timothy Herold <tim@programmersbyte.com>
2016-11-18 12:26:59 -0800

    Bind UI elements for Search activity.

--- app/src/main/java/com/programmersbyte/politicalaction/SearchCongressActivity.java
+++ app/src/main/java/com/programmersbyte/politicalaction/SearchCongressActivity.java
@@ -2,12 +2,20 @@ package com.programmersbyte.politicalaction;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class SearchCongressActivity extends AppCompatActivity {
+    @Bind(R.id.editSearchCongress) EditText mEditSearchCongress;
+    @Bind(R.id.buttonSubmitSearch) Button mButtonSubmitSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_congress);
+        ButterKnife.bind(this);
     }
 }
