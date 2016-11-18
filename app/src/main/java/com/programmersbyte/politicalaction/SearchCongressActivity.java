70456d6e1c9223bae4ac155d0026f34181d0ef21
Timothy Herold <tim@programmersbyte.com>
2016-11-18 10:18:59 -0800

    Add empty activities for viewing all congressmen, viewing all senators, and searching congress.

---
+++ app/src/main/java/com/programmersbyte/politicalaction/SearchCongressActivity.java
@@ -0,0 +1,13 @@
+package com.programmersbyte.politicalaction;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SearchCongressActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_congress);
+    }
+}
