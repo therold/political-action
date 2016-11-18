c5c7a5d695d38e59ce135721bb5c6d22777e1757
Timothy Herold <tim@programmersbyte.com>
2016-11-18 10:15:29 -0800

    Implement View.OnClickListener in MainActivity. Currently do nothing inside onClick method. TODO: Actually switch activites when TextView links are clicked.

--- app/src/main/java/com/programmersbyte/politicalaction/MainActivity.java
+++ app/src/main/java/com/programmersbyte/politicalaction/MainActivity.java
@@ -2,12 +2,13 @@ package com.programmersbyte.politicalaction;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @Bind(R.id.textAllCongressmenLink) TextView mAllCongressmenLink;
     @Bind(R.id.textAllSenatorsLink) TextView mAllSenatorsLink;
     @Bind(R.id.textSearchCongressLink) TextView mSearchCongressLink;
@@ -17,5 +18,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == mAllCongressmenLink) {
+            // Go to Activity
+        } else if(view == mAllSenatorsLink) {
+            // Go to Activity
+        } else if(view == mSearchCongressLink) {
+            // Go to Activity
+        }
     }
 }
