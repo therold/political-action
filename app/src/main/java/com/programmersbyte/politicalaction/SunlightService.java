5c96cd9a3573e044e68e7260b7266522103808b3
Timothy Herold <tim@programmersbyte.com>
2016-11-29 15:56:50 -0800

    Add processResults() method. Method converts raw JSON result from Sunlight Congress into an ArrayList of Legislator objects.

--- app/src/main/java/com/programmersbyte/politicalaction/SunlightService.java
+++ app/src/main/java/com/programmersbyte/politicalaction/SunlightService.java
@@ -1,11 +1,18 @@
 package com.programmersbyte.politicalaction;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.Response;
 
 public class SunlightService {
     public static void findLegislators(String chamber, Callback callback){
@@ -24,5 +31,60 @@ public class SunlightService {
 
         Call call = client.newCall(request);
         call.enqueue(callback);
+    }
+
+    public ArrayList<Legislator> processResults(Response response) {
+        ArrayList<Legislator> legislators = new ArrayList<>();
+
+        try {
+            String jsonData = response.body().string();
+            if(response.isSuccessful()) {
+                JSONObject sunlightJSON = new JSONObject(jsonData);
+                JSONArray resultsJSON = sunlightJSON.getJSONArray("results");
+                for(int i = 0; i < resultsJSON.length(); i++) {
+                    JSONObject legislatorJSON = resultsJSON.getJSONObject(i);
+                    String bioguideId = legislatorJSON.getString("bioguide_id");
+                    boolean inOffice = legislatorJSON.getBoolean("in_office");
+                    String party = legislatorJSON.getString("party");
+                    String gender = legislatorJSON.getString("gender");
+                    String state = legislatorJSON.getString("state");;
+                    String stateName = legislatorJSON.getString("state_name");
+                    Integer district = legislatorJSON.optInt("district");
+                    String title = legislatorJSON.getString("title");
+                    String chamber = legislatorJSON.getString("chamber");
+                    Integer senateClass = legislatorJSON.optInt("senate_class");
+                    String stateRank = legislatorJSON.optString("senate_rank");
+                    String birthday = legislatorJSON.getString("birthday");
+                    String termStart = legislatorJSON.getString("term_start");
+                    String termEnd = legislatorJSON.getString("term_end");
+                    String firstName = legislatorJSON.getString("first_name");
+                    String middleName = legislatorJSON.getString("middle_name");
+                    String lastName = legislatorJSON.getString("last_name");
+                    String nickname = legislatorJSON.getString("nickname");
+                    String nameSuffix = legislatorJSON.getString("name_suffix");
+                    String phone = legislatorJSON.getString("phone");
+                    String website = legislatorJSON.getString("website");
+                    String office = legislatorJSON.getString("office");
+                    String contactForm = legislatorJSON.optString("contact_form");
+                    String fax = legislatorJSON.optString("fax");
+                    String twitterId = legislatorJSON.optString("twitter_id");
+                    String youtubeId = legislatorJSON.optString("youtube_id");
+                    String facebookId = legislatorJSON.optString("facebook_id");
+
+                    Legislator legislator = new Legislator(bioguideId, inOffice, party, gender, state,
+                        stateName, district, title, chamber, senateClass, stateRank, birthday, termStart,
+                        termEnd, firstName, middleName, lastName, nickname, nameSuffix, phone, website,
+                        office, contactForm, fax, twitterId, youtubeId, facebookId);
+                    legislators.add(legislator);
+                }
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return legislators;
     }
 }
