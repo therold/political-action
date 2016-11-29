a3767105cc3cfa9230b0eeb7c6d3f1488afc22a9
Timothy Herold <tim@programmersbyte.com>
2016-11-29 14:26:26 -0800

    Add constants required to submit API requests to Sunlight Congress.

--- app/src/main/java/com/programmersbyte/politicalaction/Constants.java
+++ app/src/main/java/com/programmersbyte/politicalaction/Constants.java
@@ -2,4 +2,8 @@ package com.programmersbyte.politicalaction;
 
 public class Constants {
     public static final String SUNLIGHT_API_KEY = BuildConfig.SUNLIGHT_API_KEY;
+    public static final String SUNLIGHT_BASE_URL = "https://congress.api.sunlightfoundation.com";
+    public static final String SUNLIGHT_LEGISLATOR_BASE_PATH = "legislators";
+    public static final String SUNLIGHT_LEGISTATOR_CHAMBER_QUERY_PARAMETER = "chamber";
+    public static final String SUNLIGHT_LEGISTATOR_PAGELIMIT_QUERY_PARAMETER = "per_page";
 }
---
+++ app/src/main/java/com/programmersbyte/politicalaction/SunlightService.java
@@ -0,0 +1,7 @@
+package com.programmersbyte.politicalaction;
+
+/**
+ * Created by Guest on 11/29/16.
+ */
+public class SunlightService {
+}
