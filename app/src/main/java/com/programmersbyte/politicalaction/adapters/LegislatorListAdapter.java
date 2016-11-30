22a1d31d52b7e9f43b915ea880a2d63dd3873bac
Timothy Herold <tim@programmersbyte.com>
2016-11-30 10:48:40 -0800

    Add clickListener to LegislatorListAdapter. LegislatorListAdapter will now launch LegislatorDetailActivity on click.

--- app/src/main/java/com/programmersbyte/politicalaction/adapters/LegislatorListAdapter.java
+++ app/src/main/java/com/programmersbyte/politicalaction/adapters/LegislatorListAdapter.java
@@ -1,6 +1,7 @@
 package com.programmersbyte.politicalaction.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +11,9 @@ import android.widget.TextView;
 
 import com.programmersbyte.politicalaction.R;
 import com.programmersbyte.politicalaction.models.Legislator;
+import com.programmersbyte.politicalaction.ui.LegislatorDetailActivity;
+
+import org.parceler.Parcels;
 
 import java.util.ArrayList;
 
@@ -42,7 +46,7 @@ public class LegislatorListAdapter extends RecyclerView.Adapter<LegislatorListAd
         return mLegislators.size();
     }
 
-    public class LegislatorViewHolder extends RecyclerView.ViewHolder {
+    public class LegislatorViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         @Bind(R.id.legislatorFullNameTextView) TextView mFullNameText;
         @Bind(R.id.partyTextView) TextView mPartyText;
         @Bind(R.id.chamberTextView) TextView mChamberText;
@@ -53,6 +57,16 @@ public class LegislatorListAdapter extends RecyclerView.Adapter<LegislatorListAd
             super(itemView);
             ButterKnife.bind(this, itemView);
             mContext = itemView.getContext();
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            int itemPosition = getLayoutPosition();
+            Intent intent = new Intent(mContext, LegislatorDetailActivity.class);
+            intent.putExtra("position", itemPosition);
+            intent.putExtra("legislators", Parcels.wrap(mLegislators));
+            mContext.startActivity(intent);
         }
 
         public void bindLegislator(Legislator legislator) {
