34315344c790baf1ca7e94f7c938de590423ecdd
Timothy Herold <tim@programmersbyte.com>
2016-11-30 10:42:49 -0800

    Adjust LegislatorDetailFragment to display legislator details from web API instead of placeholder data.

--- app/src/main/java/com/programmersbyte/politicalaction/ui/LegislatorDetailFragment.java
+++ app/src/main/java/com/programmersbyte/politicalaction/ui/LegislatorDetailFragment.java
@@ -6,23 +6,59 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.parceler.Parcels;
 
 import com.programmersbyte.politicalaction.R;
+import com.programmersbyte.politicalaction.models.Legislator;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 
 public class LegislatorDetailFragment extends Fragment {
+    @Bind(R.id.profileImageView) ImageView mImageLabel;
+    @Bind(R.id.legislatorNameTextView) TextView mNameLabel;
+    @Bind(R.id.partyTextView) TextView mPartyLabel;
+    @Bind(R.id.chamberTextView) TextView mChamberLabel;
+    @Bind(R.id.websiteTextView) TextView mWebsiteLabel;
+    @Bind(R.id.phoneTextView) TextView mPhoneLabel;
+    @Bind(R.id.addressTextView) TextView mAddressLabel;
+    @Bind(R.id.saveLegislatorButton) Button mSaveLegislatorButton;
 
+    private Legislator mLegislator;
 
-    public LegislatorDetailFragment() {
-        // Required empty public constructor
+    public static LegislatorDetailFragment newInstance(Legislator legislator) {
+        LegislatorDetailFragment legislatorDetailFragment = new LegislatorDetailFragment();
+        Bundle args = new Bundle();
+        args.putParcelable("legislator", Parcels.wrap(legislator));
+        legislatorDetailFragment.setArguments(args);
+        return legislatorDetailFragment;
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mLegislator = Parcels.unwrap(getArguments().getParcelable("legislator"));
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_legislator_detail, container, false);
+        View view = inflater.inflate(R.layout.fragment_legislator_detail, container, false);
+        ButterKnife.bind(this, view);
+
+        mNameLabel.setText(mLegislator.getFullName());
+        mPartyLabel.setText(mLegislator.getParty());
+        mChamberLabel.setText(mLegislator.getChamber());
+        mWebsiteLabel.setText(mLegislator.getWebsite());
+        mPhoneLabel.setText(mLegislator.getPhone());
+        mAddressLabel.setText(mLegislator.getOffice());
+
+        return view;
     }
 
 }
