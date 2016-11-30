a107e563d82df623a6b446d0de42fa7a157a32c6
Timothy Herold <tim@programmersbyte.com>
2016-11-30 13:48:25 -0800

    Transition SearchResultsActivity from hard-coded array of legislators to live web API calls via SunlightService.

--- app/src/main/java/com/programmersbyte/politicalaction/ui/SearchResultsActivity.java
+++ app/src/main/java/com/programmersbyte/politicalaction/ui/SearchResultsActivity.java
@@ -3,23 +3,34 @@ package com.programmersbyte.politicalaction.ui;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.programmersbyte.politicalaction.Congress;
 import com.programmersbyte.politicalaction.R;
+import com.programmersbyte.politicalaction.adapters.LegislatorListAdapter;
+import com.programmersbyte.politicalaction.models.Legislator;
+import com.programmersbyte.politicalaction.services.SunlightService;
 
 import org.w3c.dom.Text;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class SearchResultsActivity extends AppCompatActivity {
-    @Bind(R.id.textSearchName) TextView mTextSearchName;
-    @Bind(R.id.textSearchError) TextView mTextSearchError;
-    @Bind(R.id.searchListView) ListView mSearchListView;
-    private String[] mSearchResults;
+
+    @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
+    private LegislatorListAdapter mAdapter;
+    public ArrayList<Legislator> mLegislators = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,12 +42,33 @@ public class SearchResultsActivity extends AppCompatActivity {
         String name = intent.getStringExtra("name");
         setTitle(getString(R.string.search_results_title, name));
 
-        mSearchResults = Congress.search(name);
-        if(mSearchResults.length < 1) {
-            mTextSearchError.setText(getString(R.string.search_no_results));
-        } else {
-            ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, mSearchResults);
-            mSearchListView.setAdapter(adapter);
-        }
+        getLegislators(name);
+    }
+
+    private void getLegislators(String query) {
+        final SunlightService sunlightService = new SunlightService();
+        sunlightService.queryLegislators(query, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) {
+                mLegislators = sunlightService.processResults(response);
+                SearchResultsActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mAdapter = new LegislatorListAdapter(getApplicationContext(), mLegislators);
+                        mRecyclerView.setAdapter(mAdapter);
+                        RecyclerView.LayoutManager layoutManager =
+                                new LinearLayoutManager(SearchResultsActivity.this);
+                        mRecyclerView.setLayoutManager(layoutManager);
+                        mRecyclerView.setHasFixedSize(true);
+
+                    }
+                });
+            }
+        });
     }
 }
